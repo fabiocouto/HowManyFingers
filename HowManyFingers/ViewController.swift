3fdaeaccd684c82afba58c5fa0f715b011a03df5
fabiodocoutooliveira <fabio.fco@gmail.com>
2017-06-25 19:21:22 -0300

    Initial Commit

---
+++ HowManyFingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowManyFingers
+//
+//  Created by Fábio do Couto Oliveira on 6/25/17.
+//  Copyright © 2017 Fábio do Couto Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
