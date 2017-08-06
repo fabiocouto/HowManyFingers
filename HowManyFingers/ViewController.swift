5830f7048c1960d2e1878c0d1d233cb2663664ef
fabiodocoutooliveira <fabio.fco@gmail.com>
2017-08-06 02:00:16 -0300

    How Many Fingers
    Initial commit.

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textGuess: UITextField!
+    
+    @IBOutlet weak var guessResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBAction func guessAction(_ sender: Any) {
+      print("button pressed!")
+      compareResult()
+        
+    }
+    
+   
+    func compareResult(){
+    
+        let fingers:UInt32 = arc4random_uniform(6)
+        let input = Int(textGuess.text!)
+        
+        if input! > 0 {
+    
+            if (Int(fingers) == input){
+        
+            guessResult.text = "Yeah! You are right!!!"
+            guessResult.textColor = UIColor.green
+        
+            } else {
+        
+            guessResult.text = "Wrong! It was a\(fingers). Try it again!"
+            guessResult.textColor = UIColor.red
+        
+        }
+    }
+        
+    
+    }
+    
 
 }
 
